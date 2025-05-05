83f6246b4b7eca8b25891e7020e3d18e92b41004
illusoryday <455248805@qq.com>
2025-05-05 12:31:01 +0800

    2025/5/5 12:30

--- include/Search/Search.h
+++ include/Search/Search.h
@@ -1,22 +1,35 @@
 #include <httplib/httplib.h>
-#include <simdjson.h>
+#include <simdjson/simdjson.h>
 #include <string.h>
 #include <iostream>
 #include <vector>
+#include <utility>
 using namespace std;
 using namespace simdjson;
 using namespace httplib;
 struct MusicInfo {
-    string title;
-    string artist;
-    string album;
-    string url;     // 下载URL
-    string source;  // 来源平台
+    string title       //歌名
+         , artist      //歌手
+         , album       //专辑
+         , musicurl    //歌曲URL
+         , lyricsurl   //歌词URL
+         , source;     // 来源平台
 };
 
-vector<string_view> GetAPI() {
-    vector<string_view> API;
-    ondemand::parser parser;
-    padded_string json = padded_string::load("./API/API.json");
-    return API;
+pair<vector<string>, vector<string>> GetAPI() {
+    pair<vector<string>, vector<string>> APIList;
+    dom::parser parser;
+    try {
+        padded_string json = padded_string::load("./API/API.json");
+        dom::element data = parser.parse(json);
+        simdjson::dom::array apis = data["APIs"].get_array();
+        for (auto api : apis) {
+            simdjson::dom::object api_obj = api.get_object();
+            for (auto [name, url] : api_obj) {
+                APIList.second.push_back(string(name));
+                APIList.first.push_back(string(url.get_string().value()));
+            }
+        }
+    }catch(const simdjson_error& e) { return { {}, {} }; }
+    return APIList;
 }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "TheSinger.h"
 #include <QtWidgets/QApplication>
 #include <httplib/httplib.h>
+#include <search/search.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     TheSinger w;
     w.show();
+    GetAPI();
     return a.exec();
 }
